bd7eeacebb7890052fc7873a8b3ae8856652498f
David Dollar <ddollar@gmail.com>
2009-09-22 15:33:10 -0400

    initial commit

---
+++ BlankerView.h
@@ -0,0 +1,17 @@
+//
+//  BlankerView.h
+//  Blanker
+//
+//  Created by David Dollar on 9/22/09.
+//  Copyright (c) 2009, Emcien. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+
+@interface BlankerView : ScreenSaverView 
+{
+
+}
+
+@end
---
+++ BlankerView.m
@@ -0,0 +1,53 @@
+//
+//  BlankerView.m
+//  Blanker
+//
+//  Created by David Dollar on 9/22/09.
+//  Copyright (c) 2009, Emcien. All rights reserved.
+//
+
+#import "BlankerView.h"
+
+
+@implementation BlankerView
+
+- (id)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
